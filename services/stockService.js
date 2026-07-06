500d901d16a1e4c2537efb8d78395e1e1fa3c343
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-06 23:25:08 +0700

    Update stockService.js

--- services/stockService.js
+++ services/stockService.js
@@ -1,24 +1,9 @@
 import yahooFinance from "yahoo-finance2";
 
+console.log("YahooFinance =", yahooFinance);
+
 export async function getStockData(kode) {
-
-  const symbol = `${kode}.JK`;
-
-  const result = await yahooFinance.chart(symbol, {
-    period1: new Date(Date.now() - 120 * 24 * 60 * 60 * 1000),
-    interval: "1d"
-  });
-
-  if (!result?.quotes?.length) {
-    throw new Error(`Data saham ${kode} tidak ditemukan.`);
-  }
-
-  const closePrices = result.quotes
-    .map(q => q.close)
-    .filter(v => typeof v === "number");
-
-  return {
-    kode,
-    closePrices
-  };
+  throw new Error(
+    "Keys: " + Object.keys(yahooFinance).join(", ")
+  );
 }
