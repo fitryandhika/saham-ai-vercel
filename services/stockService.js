ee86c23959d626fecc87bfd098ffef9770a7db26
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-06 23:18:49 +0700

    Update stockService.js

--- services/stockService.js
+++ services/stockService.js
@@ -1,13 +1,11 @@
-import YahooFinance from "yahoo-finance2";
-
-const yahooFinance = new YahooFinance();
+import yahooFinance from "yahoo-finance2";
 
 export async function getStockData(kode) {
 
   const symbol = `${kode}.JK`;
 
   const history = await yahooFinance.historical(symbol, {
-    period1: new Date(Date.now() - 1000 * 60 * 60 * 24 * 120),
+    period1: new Date(Date.now() - 120 * 24 * 60 * 60 * 1000),
     interval: "1d"
   });
 
@@ -17,7 +15,7 @@ export async function getStockData(kode) {
 
   const closePrices = history
     .map(item => item.close)
-    .filter(price => price !== null);
+    .filter(price => price != null);
 
   return {
     kode,
