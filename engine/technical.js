f90a3d917e80dcd99e3a173dbe75fb608eef8c12
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-06 17:34:43 +0700

    Update technical.js

--- engine/technical.js
+++ engine/technical.js
@@ -20,3 +20,30 @@ export function calculateEMA(prices, period) {
 
   return Number(ema.toFixed(2));
 }
+export function calculateRSI(prices, period = 14) {
+
+  if (prices.length <= period) return null;
+
+  let gain = 0;
+  let loss = 0;
+
+  for (let i = prices.length - period; i < prices.length; i++) {
+
+    const diff = prices[i] - prices[i - 1];
+
+    if (diff > 0) {
+      gain += diff;
+    } else {
+      loss -= diff;
+    }
+  }
+
+  const avgGain = gain / period;
+  const avgLoss = loss / period;
+
+  if (avgLoss === 0) return 100;
+
+  const rs = avgGain / avgLoss;
+
+  return Number((100 - (100 / (1 + rs))).toFixed(2));
+}
