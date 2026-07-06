49b064ebd76524e156cfe5eba487ce65384ea85b
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-06 17:51:01 +0700

    Update analyzer.js

--- engine/analyzer.js
+++ engine/analyzer.js
@@ -9,10 +9,19 @@ import {
   recommendation
 } from "./scorer.js";
 
+import {
+  calculateSupport,
+  calculateResistance,
+  calculateStopLoss,
+  calculateTakeProfit,
+  calculateRiskReward
+} from "./risk.js";
+
 export function analyzeStock(data) {
 
   const close = data.closePrices.at(-1);
 
+  // Technical Indicators
   const sma20 = calculateSMA(data.closePrices, 20);
   const sma50 = calculateSMA(data.closePrices, 50);
 
@@ -21,6 +30,7 @@ export function analyzeStock(data) {
 
   const rsi = calculateRSI(data.closePrices);
 
+  // AI Score
   const score = calculateScore({
     close,
     sma20,
@@ -33,16 +43,44 @@ export function analyzeStock(data) {
 
   const signal = recommendation(score);
 
+  // Risk Management
+  const support = calculateSupport(data.closePrices);
+
+  const resistance = calculateResistance(data.closePrices);
+
+  const stopLoss = calculateStopLoss(support);
+
+  const takeProfit = calculateTakeProfit(resistance);
+
+  const riskReward = calculateRiskReward(
+    close,
+    stopLoss,
+    takeProfit
+  );
+
   return {
     kode: data.kode,
     close,
+
     sma20,
     sma50,
+
     ema9,
     ema20,
+
     rsi,
+
     score,
     signal,
+
+    support,
+    resistance,
+
+    stopLoss,
+    takeProfit,
+
+    riskReward,
+
     timestamp: new Date().toISOString()
   };
 }
