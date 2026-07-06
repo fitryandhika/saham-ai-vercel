973e0c5e8ce95f608a48740332fe90cf00017061
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-06 15:00:17 +0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,11 +1,9 @@
-module.exports = async (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-
-  return res.status(200).json({
+export default function handler(req, res) {
+  res.status(200).json({
     success: true,
     service: "SahamAI API",
     version: "1.0.0",
     status: "ONLINE",
     timestamp: new Date().toISOString()
   });
-};
+}
