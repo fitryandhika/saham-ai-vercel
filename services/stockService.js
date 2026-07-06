24c2222663ac527835371d7d8d22f18037778879
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-06 23:28:46 +0700

    Update stockService.js

--- services/stockService.js
+++ services/stockService.js
@@ -1,9 +1,30 @@
-import yahooFinance from "yahoo-finance2";
-
-console.log("YahooFinance =", yahooFinance);
-
 export async function getStockData(kode) {
-  throw new Error(
-    "Keys: " + Object.keys(yahooFinance).join(", ")
-  );
+
+  const symbol = `${kode}.JK`;
+
+  const url =
+    `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?range=6mo&interval=1d`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Gagal mengambil data Yahoo Finance.");
+  }
+
+  const json = await response.json();
+
+  const result = json.chart.result?.[0];
+
+  if (!result) {
+    throw new Error(`Data ${kode} tidak ditemukan.`);
+  }
+
+  const closePrices =
+    result.indicators.quote[0].close
+      .filter(price => price !== null);
+
+  return {
+    kode,
+    closePrices
+  };
 }
