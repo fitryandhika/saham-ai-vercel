0cebbfe2492d23c49eb2b2a61b0d0a2b7789706d
fitryandhika <59013730+fitryandhika@users.noreply.github.com>
2026-07-14 13:28:03 +0700

    Delete health.js

--- api/health.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    service: "SahamAI API",
-    version: "1.0.0",
-    status: "ONLINE",
-    timestamp: new Date().toISOString()
-  });
-}
